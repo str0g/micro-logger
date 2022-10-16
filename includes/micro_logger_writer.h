c32f4e6508178cc2424d4688dc3aa550fa046b80
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2022-10-16 09:16:16 +0200

    update license information

--- includes/micro_logger_writer.h
+++ includes/micro_logger_writer.h
@@ -1,6 +1,8 @@
-//
-// Created by lukasz on 14.10.2022.
-//
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 #ifndef MICRO_LOGGER_MICRO_LOGGER_WRITER_H
 #define MICRO_LOGGER_MICRO_LOGGER_WRITER_H
