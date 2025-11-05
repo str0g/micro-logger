d789c2175b850f27304be69b2b657710c031cfc8
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2025-11-05 12:03:59 +0100

    backport folder has been renamed to micro_logger

--- cmake/other_tests.cmake
+++ cmake/other_tests.cmake
@@ -1,11 +1,9 @@
 function(custom_test_app test_name)
-    add_executable(${test_name} tests/${test_name}.cpp)
-    SET(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS}")
+    add_executable(${test_name} demos/${test_name}.cpp)
     target_link_libraries(${test_name} LINK_PUBLIC ${PROJECT_NAME} -lpthread)
 endfunction()
 
-
-function(custom_c_test test_name)
-    add_executable(${test_name} ../tests/${test_name}.c)
-    target_link_libraries(${test_name} LINK_PUBLIC ${PROJECT_NAME} -lpthread)
+function(custom_app_test_c test_name)
+    add_executable(${test_name}_c demos/${test_name}.c)
+    target_link_libraries(${test_name}_c LINK_PUBLIC ${PROJECT_NAME} -lpthread)
 endfunction()
