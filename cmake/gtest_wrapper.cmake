08fe903d66e58f659d5aad6c16127c7786ec3cd2
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2026-02-10 17:06:50 +0100

    cmake formatting

--- cmake/gtest_wrapper.cmake
+++ cmake/gtest_wrapper.cmake
@@ -1,19 +1,10 @@
 include(GoogleTest)
 
 function(custom_gtest test_name)
-    add_executable(${test_name} tests/${test_name}.cpp)
-    target_link_libraries(${test_name}
-      PRIVATE
-        ${PROJECT_NAME}
-        pthread
-        -lgtest
-        -lgtest_main
-    )
-    target_include_directories(${test_name}
-      PRIVATE
-        ${CMAKE_CURRENT_SOURCE_DIR}/src
-    )
-    gtest_add_tests(
-      TARGET ${test_name}
-    )
+  add_executable(${test_name} tests/${test_name}.cpp)
+  target_link_libraries(${test_name} PRIVATE ${PROJECT_NAME} pthread -lgtest
+                                             -lgtest_main)
+  target_include_directories(${test_name}
+                             PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/src)
+  gtest_add_tests(TARGET ${test_name})
 endfunction()
