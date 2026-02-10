59da48b053df15544001c02e4856ac264956bc14
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2026-02-10 17:01:58 +0100

    enforced name convention cmake variables

--- cmake/gtest_wrapper.cmake
+++ cmake/gtest_wrapper.cmake
@@ -2,7 +2,13 @@ include(GoogleTest)
 
 function(custom_gtest test_name)
     add_executable(${test_name} tests/${test_name}.cpp)
-    target_link_libraries(${test_name} LINK_PUBLIC ${PROJECT_NAME} -lpthread -lgtest -lgtest_main)
+    target_link_libraries(${test_name}
+      PRIVATE
+        ${PROJECT_NAME}
+        pthread
+        -lgtest
+        -lgtest_main
+    )
     target_include_directories(${test_name}
       PRIVATE
         ${CMAKE_CURRENT_SOURCE_DIR}/src
